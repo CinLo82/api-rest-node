dc7a68bd7f22dab19d22bdf0eadc94ce0f3d17ae
CinLo82 <cinlos3382@gmail.com>
2024-08-05 02:17:31 -0300

    creacion de rutas

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 const connection = require('./database/connection.js')
+const express = require('express')
+const cord = require('cors')
 
 //inicializar app
 console.log('app de node arrancada')
 
 // conectar a la base de dato
 connection()
+
+// crear servidor de node
+const app = express()
+const PORT = process.env.PORT || 4000
+
+// configurar cors
+app.use(cord())
+
+// convertir body a objeto js
+app.use(express.json())
+
+// crear rutas
+app.get('/probando', (req, res) => {
+    res.json([{
+        curso:"react",
+        precio: 50,
+        descripcion: "curso de react"
+    },
+    {
+        curso:"vue",
+        precio: 50,
+        descripcion: "curso de vue"
+    },
+    {
+        curso:"angular",
+        precio: 50,
+        descripcion: "curso de angular"
+    }])
+})
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Blog de tecnologia</h1>`)
+})
+
+// puerto de la app
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+    })
